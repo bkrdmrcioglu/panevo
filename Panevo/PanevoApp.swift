c853865f59ac27f4c5fa8e5448aaeb76027584aa
Bekir Demircioglu <bkrdmrcioglu@gmail.com>
2026-07-11 18:38:58 +0300

    Initial Commit

---
+++ Panevo/PanevoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PanevoApp.swift
+//  Panevo
+//
+//  Created by bkr on 11.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct PanevoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
